d7b46bbbc63397c6881212368ae6b2be5dc6f03a
YeisonIdarragaChavarria <yeisonidarraga97968@correo.itm.edu.co>
2021-09-02 17:47:36 -0500

    TodoApi

--- todozulu.Functions/TodoApi.cs
+++ todozulu.Functions/TodoApi.cs
@@ -18,10 +18,12 @@ namespace todozulu.Functions
     {
         [FunctionName(nameof(CreateTodo))]
         public static async Task<IActionResult> CreateTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "todo")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo")] HttpRequest req,
             [Table("todo", Connection = "Azure.WebJobs.Extensions.Storage")] CloudTable todoTable,
             ILogger log)
         {
+
+
             log.LogInformation("Receive a new todo");
 
             string name = req.Query["name"];
@@ -31,122 +33,46 @@ namespace todozulu.Functions
             Todo todo = JsonConvert.DeserializeObject<Todo>(requestBody);
 
             if (string.IsNullOrEmpty(todo?.TaskDescription))
+            {
 
                 return new BadRequestObjectResult(new Response
                 {
+
                     IsSuccess = false,
                     Message = "the requestmust have a task description"
                 });
 
-
-            {
-                TodoEntity todoEntity = new TodoEntity
-                {
-
-                    CreatedTime = DateTime.UtcNow,
-                    ETag = "*",
-                    IsCompleted = false,
-                    PartitionKey = "TODO",
-                    RowKey = Guid.NewGuid().ToString(),
-                    TaskDescription = todo.TaskDescription
-                };
-
-
-
-
-                TableOperation addOperation = TableOperation.Insert(todoEntity);
-                await todoTable.ExecuteAsync(addOperation);
-
-                string message = "New todo store in table";
-                log.LogInformation(message);
-
-                return new OkObjectResult(new Response
-                {
-
-                    IsSuccess = true,
-                    Message = message,
-                    Result = todoEntity
-
-                });
-
             }
+
+            TodoEntity todoEntity = new TodoEntity
+            {
+
+                CreatedTime = DateTime.UtcNow,
+                ETag = "*",
+                IsCompleted = false,
+                PartitionKey = "TODO",
+                RowKey = Guid.NewGuid().ToString(),
+                TaskDescription = todo.TaskDescription
+            };
+
+            TableOperation addOperation = TableOperation.Insert(todoEntity);
+            await todoTable.ExecuteAsync(addOperation);
+
+
+            string message = "New todo store in table";
+            log.LogInformation(message);
+            return new OkObjectResult(new Response
+            {
+
+                IsSuccess = true,
+                Message = message,
+                Result = todoEntity
+
+            });
+
+
+
         }
 
-        [FunctionName(nameof(UpdateTodo))]
-        public static async Task<IActionResult> UpdateTodo(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "todo/{id}")] HttpRequest req,
-            [Table("todo", Connection = "Azure.WebJobs.Extensions.Storage")] CloudTable todoTable,
-
-            string id,
-            ILogger log)
-        {
-            log.LogInformation($"update for :{id}, received");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-            Todo todo = JsonConvert.DeserializeObject<Todo>(requestBody);
-
-
-            TableOperation findOperation = TableOperation.Retrieve<TodoEntity>" TODO",id);
-            TableResult findResult = await todoTable.ExecuteAsync(findOperation);
-            if (findResult == null)
-            {
-                return new BadRequestObjectResult(new Response
-                {
-                    IsSuccess = false,
-                    Message = "Todo not found"
-                }
-                    );
-                TodoEntity todoEntity = (TodoEntity)findResult.Result;
-                todoEntity.IsCompleted = todo.IsCompleted;
-                if (string.IsNullOrEmpty(todo.TaskDescription))
-                {
-                    todoEntity.TaskDescription = todo.TaskDescription;
-                }
-
-
-            if (string.IsNullOrEmpty(todo?.TaskDescription))
-
-                return new BadRequestObjectResult(new Response
-                {
-                    IsSuccess = false,
-                    Message = "the requestmust have a task description"
-                });
-
-
-            {
-                TodoEntity todoEntity = new TodoEntity
-                {
-
-                    CreatedTime = DateTime.UtcNow,
-                    ETag = "*",
-                    IsCompleted = false,
-                    PartitionKey = "TODO",
-                    RowKey = Guid.NewGuid().ToString(),
-                    TaskDescription = todo.TaskDescription
-                };
-
-
-
-
-                TableOperation addOperation = TableOperation.Insert(todoEntity);
-                await todoTable.ExecuteAsync(addOperation);
-
-                string message = "New todo store in table";
-                log.LogInformation(message);
-
-                return new OkObjectResult(new Response
-                {
-
-                    IsSuccess = true,
-                    Message = message,
-                    Result = todoEntity
-
-                });
-
-            }
-        }
     }
 }
